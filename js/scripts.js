35fe45b50e7eadff01abce8175d7c6b01fc1df47
André Luis Fernandes <62942316+Andre-StudioNerd@users.noreply.github.com>
2025-02-13 15:25:38 -0300

    Segundo Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 const apiKey = "7800ac721723fdccad438ac75b7b4f59";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
-
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
-
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
@@ -12,12 +10,9 @@ const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const umidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
-
 const weatherContainer = document.querySelector("#weather-data");
-
 const errorMessageContainer = document.querySelector("#error-message");
 const loader = document.querySelector("#loader");
-
 const suggestionContainer = document.querySelector("#suggestions");
 const suggestionButtons = document.querySelectorAll("#suggestions button");
 
@@ -68,12 +63,11 @@ const showWeatherData = async (city) => {
     "src",
     `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
   );
- 
+
   umidityElement.innerText = `${data.main.humidity}%`;
   windElement.innerText = `${data.wind.speed}km/h`;
 
   // Change bg image
-  
 
   weatherContainer.classList.remove("hide");
 };
